bfbd47a93555dec08f016c20d04871ec1bcc1ca5
Innula2 <91963982+Innula2@users.noreply.github.com>
2022-04-03 23:34:31 +0300

    Добавьте файлы проекта.

---
+++ lab8.3/ApplicationContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+
+namespace lab8._3
+{
+    public class ApplicationContext : DbContext
+
+ {
+        public DbSet<Company> Companies { get; set; }
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.
+
+           UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=relationsdb7;Trusted_Connection=True;");
+        }
+    }
+
+}
---
+++ lab8.3/Company.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace lab8._3
+{
+    public class Company
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public List<User> Users { get; set; } = new List<User>();
+    }
+}
